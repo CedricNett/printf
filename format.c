bc4408f4329205eb52848457857897adac25f1e9
MatthiasH119 <matthiasheiming@aol.de>
2022-05-14 11:01:16 +0200

    1. Commitversuch vom Matti; Branch: mh/format

---
+++ format.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+
+int main () {
+        int Ganzzahl = 12345;
+        float Gleitkommazahl = 123.45;
+        char Character = 'a';
+        char String[]="Das ist ein String";
+
+        printf("Ganzzahl: %i \n", Ganzzahl);
+        printf("Gleitkommazahl: %.2f\n", Gleitkommazahl);
+        printf("Character: %c \n", Character);
+        printf("String: %s \n", String);
+
+        return 0;
+}
